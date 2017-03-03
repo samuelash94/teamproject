a3a55d84888bf2feeb1c0ae74a3b9fdc0d7fb231
samuelash94 <samuelash94@gmail.com>
2017-03-03 11:38:01 -0330

    Updated index.js. Added posts.js and post.js

---
+++ project/models/post.js
@@ -0,0 +1,41 @@
+/**
+ * http://usejsdoc.org/
+ */
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
+
+// Post Schema
+var PostSchema = mongoose.Schema({
+	body: {
+		type: String,
+		index:true
+	},
+	date: {
+		type: Date
+	}
+	image: {
+		type: null
+	}
+});
+
+var Post = module.exports = mongoose.model('Post', PostSchema);
+
+module.exports.createPost = function(newPost, callback){
+	newPost.save(callback);
+}
+
+module.exports.getPostByID = function(id, callback){
+	Post.findById(id, callback);
+}
+/*
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	if(err) throw err;
+    	callback(null, isMatch);
+	});
+*/
+}
