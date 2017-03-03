68650ff4339e5f07aeaacfb093bb7f4eea149fc4
sraa61 <sraa61@mun.ca>
2017-03-03 10:52:47 -0330

    Updated index.hbs and index.js for purposes of posts. Not complete

--- project/routes/index.js
+++ project/routes/index.js
@@ -6,6 +6,10 @@ router.get('/', ensureAuthenticated, function(req, res){
 	res.render('index');
 });
 
+router.get('/', createPost, function(req, res){
+	res.render('index');
+});
+
 function ensureAuthenticated(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
@@ -14,5 +18,40 @@ function ensureAuthenticated(req, res, next){
 		res.redirect('/users/login');
 	}
 }
+
+function displayPostField(){
+	document.getElementById("postField").style.visibility = 'visible';
+}
+
+router.post('/post', function(req, res){
+	var postField = req.body.postField;
+	var date = new Date();
+	var day = today.getDate();
+	var month = today.getMonth()+1;
+	var year = today.getFullYear();
+	var hour = today.getHours();
+	var minute = today.getMinutes();
+	req.checkBody('postField', 'Post must not be empty').notEmpty();
+	
+	if(errors){
+		res.render('register',{
+			errors:errors
+		});
+	} else {
+		var newPost = new Post({
+			body: postField;
+			day: day;
+			month: month;
+			year: year;
+			hour: hour;
+			minute: minute;
+		});
+		
+		User.createPost(newPost, function(err, user){
+			if(err) throw err;
+			console.log(user);
+		});
+	  
+});
 
 module.exports = router;
