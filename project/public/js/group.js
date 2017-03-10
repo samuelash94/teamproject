5228882e5ec3e420d55088320ea6d6dd824de70c
samuelash94 <samuelash94@gmail.com>
2017-03-10 15:40:50 -0330

    Updated groups page

---
+++ project/public/js/group.js
@@ -0,0 +1,6 @@
+function displayGroupFields(){
+	document.getElementById("groupName").style.visibility = 'visible';
+	document.getElementById("groupDesc").style.visibility = 'visible';
+	document.getElementById("groupPrivacy").style.visibility = 'visible';
+	document.getElementById("groupSubmit").style.visibility = 'visible';
+}
