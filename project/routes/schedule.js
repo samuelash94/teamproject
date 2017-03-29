c5183a3f7cede82c5cf5341398182493f70ca5c7
jeffconway <d46jlc@mun.ca>
2017-03-29 12:13:55 -0230

    Add files via upload

--- project/routes/schedule.js
+++ project/routes/schedule.js
@@ -25,668 +25,83 @@ router.post('/create', function(req, res){
 	if(typeof mon != 'undefined'){
 		var monTime = "Monday at ";
 		var monStartHour = req.body.mondayStartHour;
-		switch(monStartHour){
-			case "H1":
-				monTime += "1:";
-				break;
-			case "H2":
-				monTime += "2:";
-				break;
-			case "H3":
-				monTime += "3:";
-				break;
-			case "H4":
-				monTime += "4:";
-				break;
-			case "H5":
-				monTime += "5:";
-				break;
-			case "H6":
-				monTime += "6:";
-				break
-			case "H7":
-				monTime += "7:";
-				break;
-			case "H8":
-				monTime += "8:";
-				break;
-			case "H9":
-				monTime += "9:";
-				break;
-			case "H10":
-				monTime += "10:";
-				break;
-			case "H11":
-				monTime += "11:";
-				break;
-			case "H12":
-				monTime += "12:";
-				break;
-		}
+		monTime += monStartHour + ":";
 		var monStartMin = req.body.mondayStartMinutes;
-		switch(monStartMin){
-			case "M1":
-				monTime += "00 ";
-				break;
-			case "M2":
-				monTime += "15 ";
-				break;
-			case "M3":
-				monTime += "30 ";
-				break;
-			case "M4":
-				monTime += "45 ";
-				break;
-		}
+		monTime += monStartMin + " ";
 		var monStartAP = req.body.mondayStartAMPM;
-		switch(monStartAP){
-			case "AM":
-				monTime += "AM - ";
-				break;
-			case "PM":
-				monTime += "PM - ";
-				break;
-		}
+		monTime += monStartAP + " - ";
 		var monEndHour = req.body.mondayEndHour;
-		switch(monEndHour){
-			case "H1":
-				monTime += "1:";
-				break;
-			case "H2":
-				monTime += "2:";
-				break;
-			case "H3":
-				monTime += "3:";
-				break;
-			case "H4":
-				monTime += "4:";
-				break;
-			case "H5":
-				monTime += "5:";
-				break;
-			case "H6":
-				monTime += "6:";
-				break
-			case "H7":
-				monTime += "7:";
-				break;
-			case "H8":
-				monTime += "8:";
-				break;
-			case "H9":
-				monTime += "9:";
-				break;
-			case "H10":
-				monTime += "10:";
-				break;
-			case "H11":
-				monTime += "11:";
-				break;
-			case "H12":
-				monTime += "12:";
-				break;
-		}
+		monTime += monEndHour + ":";
 		var monEndMin = req.body.mondayEndMinutes;
-		switch(monEndMin){
-			case "M1":
-				monTime += "00 ";
-				break;
-			case "M2":
-				monTime += "15 ";
-				break;
-			case "M3":
-				monTime += "30 ";
-				break;
-			case "M4":
-				monTime += "45 ";
-				break;
-			case "M5":
-				monTime += "50 ";
-				break;
-		}
+		monTime += monEndMin + " ";
 		var monEndAP = req.body.mondayEndAMPM;
-		switch(monEndAP){
-			case "AM":
-				monTime += "AM";
-				break;
-			case "PM":
-				monTime += "PM";
-				break;
-		}
+		monTime += monEndAP;
 		daysTimes.push(monTime);
 	}
 
 	if(typeof tues != 'undefined'){
 		var tuesTime = "Tuesday at ";
 		var tuesStartHour = req.body.tuesdayStartHour;
-		switch(tuesStartHour){
-			case "H1":
-				tuesTime += "1:";
-				break;
-			case "H2":
-				tuesTime += "2:";
-				break;
-			case "H3":
-				tuesTime += "3:";
-				break;
-			case "H4":
-				tuesTime += "4:";
-				break;
-			case "H5":
-				tuesTime += "5:";
-				break;
-			case "H6":
-				tuesTime += "6:";
-				break
-			case "H7":
-				tuesTime += "7:";
-				break;
-			case "H8":
-				tuesTime += "8:";
-				break;
-			case "H9":
-				tuesTime += "9:";
-				break;
-			case "H10":
-				tuesTime += "10:";
-				break;
-			case "H11":
-				tuesTime += "11:";
-				break;
-			case "H12":
-				tuesTime += "12:";
-				break;
-		}
+		tuesTime += tuesStartHour + ":";
 		var tuesStartMin = req.body.tuesdayStartMinutes;
-		switch(tuesStartMin){
-			case "M1":
-				tuesTime += "00 ";
-				break;
-			case "M2":
-				tuesTime += "15 ";
-				break;
-			case "M3":
-				tuesTime += "30 ";
-				break;
-			case "M4":
-				tuesTime += "45 ";
-				break;
-		}
+		tuesTime += tuesStartMin + " ";
 		var tuesStartAP = req.body.tuesdayStartAMPM;
-		switch(tuesStartAP){
-			case "AM":
-				tuesTime += "AM - ";
-				break;
-			case "PM":
-				tuesTime += "PM - ";
-				break;
-		}
+		tuesTime += tuesStartAP + " - ";
 		var tuesEndHour = req.body.tuesdayEndHour;
-		switch(tuesEndHour){
-			case "H1":
-				tuesTime += "1:";
-				break;
-			case "H2":
-				tuesTime += "2:";
-				break;
-			case "H3":
-				tuesTime += "3:";
-				break;
-			case "H4":
-				tuesTime += "4:";
-				break;
-			case "H5":
-				tuesTime += "5:";
-				break;
-			case "H6":
-				tuesTime += "6:";
-				break
-			case "H7":
-				tuesTime += "7:";
-				break;
-			case "H8":
-				tuesTime += "8:";
-				break;
-			case "H9":
-				tuesTime += "9:";
-				break;
-			case "H10":
-				tuesTime += "10:";
-				break;
-			case "H11":
-				tuesTime += "11:";
-				break;
-			case "H12":
-				tuesTime += "12:";
-				break;
-		}
+		tuesTime += tuesEndHour + ":";
 		var tuesEndMin = req.body.tuesdayEndMinutes;
-		switch(tuesEndMin){
-			case "M1":
-				tuesTime += "00 ";
-				break;
-			case "M2":
-				tuesTime += "15 ";
-				break;
-			case "M3":
-				tuesTime += "30 ";
-				break;
-			case "M4":
-				tuesTime += "45 ";
-				break;
-			case "M5":
-				tuesTime += "50 ";
-				break;
-		}
+		tuesTime += tuesEndMin + " ";
 		var tuesEndAP = req.body.tuesdayEndAMPM;
-		switch(tuesEndAP){
-			case "AM":
-				tuesTime += "AM";
-				break;
-			case "PM":
-				tuesTime += "PM";
-				break;
-		}
+		tuesTime += tuesEndAP;
 		daysTimes.push(tuesTime);
 	}
 
 	if(typeof wed != 'undefined'){
-		var wedTime = "Wednesday at ";
+		var wedTime = "wednesday at ";
 		var wedStartHour = req.body.wednesdayStartHour;
-		switch(wedStartHour){
-			case "H1":
-				wedTime += "1:";
-				break;
-			case "H2":
-				wedTime += "2:";
-				break;
-			case "H3":
-				wedTime += "3:";
-				break;
-			case "H4":
-				wedTime += "4:";
-				break;
-			case "H5":
-				wedTime += "5:";
-				break;
-			case "H6":
-				wedTime += "6:";
-				break
-			case "H7":
-				wedTime += "7:";
-				break;
-			case "H8":
-				wedTime += "8:";
-				break;
-			case "H9":
-				wedTime += "9:";
-				break;
-			case "H10":
-				wedTime += "10:";
-				break;
-			case "H11":
-				wedTime += "11:";
-				break;
-			case "H12":
-				wedTime += "12:";
-				break;
-		}
+		wedTime += wedStartHour + ":";
 		var wedStartMin = req.body.wednesdayStartMinutes;
-		switch(wedStartMin){
-			case "M1":
-				wedTime += "00 ";
-				break;
-			case "M2":
-				wedTime += "15 ";
-				break;
-			case "M3":
-				wedTime += "30 ";
-				break;
-			case "M4":
-				wedTime += "45 ";
-				break;
-		}
+		wedTime += wedStartMin + " ";
 		var wedStartAP = req.body.wednesdayStartAMPM;
-		switch(wedStartAP){
-			case "AM":
-				wedTime += "AM - ";
-				break;
-			case "PM":
-				wedTime += "PM - ";
-				break;
-		}
+		wedTime += wedStartAP + " - ";
 		var wedEndHour = req.body.wednesdayEndHour;
-		switch(wedEndHour){
-			case "H1":
-				wedTime += "1:";
-				break;
-			case "H2":
-				wedTime += "2:";
-				break;
-			case "H3":
-				wedTime += "3:";
-				break;
-			case "H4":
-				wedTime += "4:";
-				break;
-			case "H5":
-				wedTime += "5:";
-				break;
-			case "H6":
-				wedTime += "6:";
-				break
-			case "H7":
-				wedTime += "7:";
-				break;
-			case "H8":
-				wedTime += "8:";
-				break;
-			case "H9":
-				wedTime += "9:";
-				break;
-			case "H10":
-				wedTime += "10:";
-				break;
-			case "H11":
-				wedTime += "11:";
-				break;
-			case "H12":
-				wedTime += "12:";
-				break;
-		}
+		wedTime += wedEndHour + ":";
 		var wedEndMin = req.body.wednesdayEndMinutes;
-		switch(wedEndMin){
-			case "M1":
-				wedTime += "00 ";
-				break;
-			case "M2":
-				wedTime += "15 ";
-				break;
-			case "M3":
-				wedTime += "30 ";
-				break;
-			case "M4":
-				wedTime += "45 ";
-				break;
-			case "M5":
-				monTime += "50 ";
-				break;
-		}
+		wedTime += wedEndMin + " ";
 		var wedEndAP = req.body.wednesdayEndAMPM;
-		switch(wedEndAP){
-			case "AM":
-				wedTime += "AM";
-				break;
-			case "PM":
-				wedTime += "PM";
-				break;
-		}
+		wedTime += wedEndAP;
 		daysTimes.push(wedTime);
 	}
 	if(typeof thurs != 'undefined'){
-		var thursTime = "Thrusday at ";
+		var thursTime = "thursday at ";
 		var thursStartHour = req.body.thursdayStartHour;
-		switch(thursStartHour){
-			case "H1":
-				thursTime += "1:";
-				break;
-			case "H2":
-				thursTime += "2:";
-				break;
-			case "H3":
-				thursTime += "3:";
-				break;
-			case "H4":
-				thursTime += "4:";
-				break;
-			case "H5":
-				thursTime += "5:";
-				break;
-			case "H6":
-				thursTime += "6:";
-				break
-			case "H7":
-				thursTime += "7:";
-				break;
-			case "H8":
-				thursTime += "8:";
-				break;
-			case "H9":
-				thursTime += "9:";
-				break;
-			case "H10":
-				thursTime += "10:";
-				break;
-			case "H11":
-				thursTime += "11:";
-				break;
-			case "H12":
-				thursTime += "12:";
-				break;
-		}
+		thursTime += thursStartHour + ":";
 		var thursStartMin = req.body.thursdayStartMinutes;
-		switch(thursStartMin){
-			case "M1":
-				thursTime += "00 ";
-				break;
-			case "M2":
-				thursTime += "15 ";
-				break;
-			case "M3":
-				thursTime += "30 ";
-				break;
-			case "M4":
-				thursTime += "45 ";
-				break;
-		}
+		thursTime += thursStartMin + " ";
 		var thursStartAP = req.body.thursdayStartAMPM;
-		switch(thursStartAP){
-			case "AM":
-				thursTime += "AM - ";
-				break;
-			case "PM":
-				thursTime += "PM - ";
-				break;
-		}
+		thursTime += thursStartAP + " - ";
 		var thursEndHour = req.body.thursdayEndHour;
-		switch(thursEndHour){
-			case "H1":
-				thursTime += "1:";
-				break;
-			case "H2":
-				thursTime += "2:";
-				break;
-			case "H3":
-				thursTime += "3:";
-				break;
-			case "H4":
-				thursTime += "4:";
-				break;
-			case "H5":
-				thursTime += "5:";
-				break;
-			case "H6":
-				thursTime += "6:";
-				break
-			case "H7":
-				thursTime += "7:";
-				break;
-			case "H8":
-				thursTime += "8:";
-				break;
-			case "H9":
-				thursTime += "9:";
-				break;
-			case "H10":
-				thursTime += "10:";
-				break;
-			case "H11":
-				thursTime += "11:";
-				break;
-			case "H12":
-				thursTime += "12:";
-				break;
-		}
+		thursTime += thursEndHour + ":";
 		var thursEndMin = req.body.thursdayEndMinutes;
-		switch(thursEndMin){
-			case "M1":
-				thursTime += "00 ";
-				break;
-			case "M2":
-				thursTime += "15 ";
-				break;
-			case "M3":
-				thursTime += "30 ";
-				break;
-			case "M4":
-				thursTime += "45 ";
-				break;
-			case "M5":
-				monTime += "50 ";
-				break;
-		}
+		thursTime += thursEndMin + " ";
 		var thursEndAP = req.body.thursdayEndAMPM;
-		switch(thursEndAP){
-			case "AM":
-				thursTime += "AM";
-				break;
-			case "PM":
-				thursTime += "PM";
-				break;
-		}
+		thursTime += thursEndAP;
 		daysTimes.push(thursTime);
 	}
 	if(typeof fri != 'undefined'){
-		var friTime = "Friday at ";
+		var friTime = "friday at ";
 		var friStartHour = req.body.fridayStartHour;
-		switch(friStartHour){
-			case "H1":
-				friTime += "1:";
-				break;
-			case "H2":
-				friTime += "2:";
-				break;
-			case "H3":
-				friTime += "3:";
-				break;
-			case "H4":
-				friTime += "4:";
-				break;
-			case "H5":
-				friTime += "5:";
-				break;
-			case "H6":
-				friTime += "6:";
-				break
-			case "H7":
-				friTime += "7:";
-				break;
-			case "H8":
-				friTime += "8:";
-				break;
-			case "H9":
-				friTime += "9:";
-				break;
-			case "H10":
-				friTime += "10:";
-				break;
-			case "H11":
-				friTime += "11:";
-				break;
-			case "H12":
-				friTime += "12:";
-				break;
-		}
-		var friStartMin = req.body.friStartMinutes;
-		switch(friStartMin){
-			case "M1":
-				friTime += "00 ";
-				break;
-			case "M2":
-				friTime += "15 ";
-				break;
-			case "M3":
-				friTime += "30 ";
-				break;
-			case "M4":
-				friTime += "45 ";
-				break;
-		}
+		friTime += friStartHour + ":";
+		var friStartMin = req.body.fridayStartMinutes;
+		friTime += friStartMin + " ";
 		var friStartAP = req.body.fridayStartAMPM;
-		switch(friStartAP){
-			case "AM":
-				friTime += "AM - ";
-				break;
-			case "PM":
-				friTime += "PM - ";
-				break;
-		}
+		friTime += friStartAP + " - ";
 		var friEndHour = req.body.fridayEndHour;
-		switch(friEndHour){
-			case "H1":
-				friTime += "1:";
-				break;
-			case "H2":
-				friTime += "2:";
-				break;
-			case "H3":
-				friTime += "3:";
-				break;
-			case "H4":
-				friTime += "4:";
-				break;
-			case "H5":
-				friTime += "5:";
-				break;
-			case "H6":
-				friTime += "6:";
-				break
-			case "H7":
-				friTime += "7:";
-				break;
-			case "H8":
-				friTime += "8:";
-				break;
-			case "H9":
-				friTime += "9:";
-				break;
-			case "H10":
-				friTime += "10:";
-				break;
-			case "H11":
-				friTime += "11:";
-				break;
-			case "H12":
-				friTime += "12:";
-				break;
-		}
+		friTime += friEndHour + ":";
 		var friEndMin = req.body.fridayEndMinutes;
-		switch(friEndMin){
-			case "M1":
-				friTime += "00 ";
-				break;
-			case "M2":
-				friTime += "15 ";
-				break;
-			case "M3":
-				friTime += "30 ";
-				break;
-			case "M4":
-				friTime += "45 ";
-				break;
-			case "M5":
-				monTime += "50 ";
-				break;
-		}
+		friTime += friEndMin + " ";
 		var friEndAP = req.body.fridayEndAMPM;
-		switch(friEndAP){
-			case "AM":
-				friTime += "AM";
-				break;
-			case "PM":
-				friTime += "PM";
-				break;
-		}
+		friTime += friEndAP;
 		daysTimes.push(friTime);
 	}
 
